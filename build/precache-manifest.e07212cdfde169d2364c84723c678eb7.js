562de3ba03eea9f9c7f504fa1c1e6776ea756528
Adriano Regis <adbrum@outlook.com>
2020-08-29 20:19:56 +0100

    GitHub Pages

---
+++ build/precache-manifest.e07212cdfde169d2364c84723c678eb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42639186ac1760805676661b85128eaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49b8fcefc01664db3184",
+    "url": "/static/js/2.f971d296.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f971d296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67668985f8a93d7d350",
+    "url": "/static/js/main.b35bade6.chunk.js"
+  },
+  {
+    "revision": "16aa3587be02034e6f3b",
+    "url": "/static/js/runtime-main.9adf9e3f.js"
+  }
+]);
