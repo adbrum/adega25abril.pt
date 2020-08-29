619798ee11ff661249039e16da97bdcd7574bb6a
Adriano Regis <adbrum@outlook.com>
2020-08-29 19:40:02 +0100

    Updates

---
+++ precache-manifest.b167cdc9f8f833db323f86adcc96cd78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c3b846534aa7394255a0106f1b85be",
+    "url": "/adega25abril.pt/index.html"
+  },
+  {
+    "revision": "49b8fcefc01664db3184",
+    "url": "/adega25abril.pt/static/js/2.f971d296.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adega25abril.pt/static/js/2.f971d296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23364a2f5109724c86c6",
+    "url": "/adega25abril.pt/static/js/main.2fdd6434.chunk.js"
+  },
+  {
+    "revision": "327b3aeff220522ac5de",
+    "url": "/adega25abril.pt/static/js/runtime-main.6501c051.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b46f461328ce21f9806b2f3bb23eb0b0.js"
+  "/adega25abril.pt/precache-manifest.b167cdc9f8f833db323f86adcc96cd78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adega25abril.pt/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
