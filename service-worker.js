f93896d94c2463c9225e2803870395e70c4de5cb
Adriano Regis <adbrum@outlook.com>
2020-08-30 12:21:01 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e07212cdfde169d2364c84723c678eb7.js"
+  "/precache-manifest.ca89afa1aa4894deff3813a3ea730dcf.js"
 );
 
 self.addEventListener('message', (event) => {
